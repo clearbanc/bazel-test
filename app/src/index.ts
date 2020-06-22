48003686f2429a3bd7d17cab2e5fb64bb8eb1474
Igor Barsi <ibarsi92@gmail.com>
2020-06-22 08:49:41 -0400

    ✨ Convert test app to a web server

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,17 @@
+import * as Koa from 'koa';
 import { get } from 'lodash';
 
 import { log } from '@clearbanc/logger';
 const { trim } = require('bazel_test/app/src/utils');
 const foo = require('bazel_test/app/data/foo.json');
 
-log(trim(get(foo, 'bar')));
+const BAR = trim(get(foo, 'bar'));
+
+const app = new Koa();
+
+app.use((ctx) => {
+  log('=== LOG ===', BAR);
+  ctx.body = BAR;
+});
+
+app.listen(3000);
